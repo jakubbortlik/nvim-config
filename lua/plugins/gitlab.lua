9e27f6754799d4a915906be51b63631eda51a9f6
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-02-12 12:58:11 +0100

    feat: update gitlab.nvim settings

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -3,7 +3,6 @@ local vmap = require("utils").vmap
 
 return {
   "harrisoncramer/gitlab.nvim",
-  branch = "adding-winbar",
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
@@ -20,23 +19,29 @@ return {
       },
     })
     local gitlab = require("gitlab")
+    local gitlab_server = require("gitlab.server")
     gitlab.setup({
       debug = { go_request = true, go_response = true }, -- Which values to log
+      reviewer_settings = {
+        diffview = {
+          imply_local = true,
+        },
+      },
       discussion_tree = {
         size = "25%",                                    -- Size of split
         position = "bottom",
         toggle_node = "i",
         toggle_resolved = "s",
-        tree_type = "by_file_name",
       },
       discussion_sign_and_diagnostic = {
-        skip_resolved_discussion = false,
+        skip_resolved_discussion = true,
       },
       popup = {
         exit = "q",
         perform_action = "ZZ",
-        perform_linewise_action = "<leader>l",
         opacity = 0.85,
+        width = "60%",
+        backup_register = "+",
       },
     })
     local create_mappings = function()
@@ -82,5 +87,11 @@ return {
     nmap("glp", gitlab.pipeline, "Gitlab Pipeline")
     nmap("glo", gitlab.open_in_browser, "Gitlab Open in browser")
     nmap("glm", gitlab.move_to_discussion_tree_from_diagnostic, "Move to discussion")
+    nmap("gl<C-r>", function()
+      gitlab_server.restart(function()
+        vim.cmd.tabclose()
+        gitlab.review()
+      end)
+    end, "Gitlab Restart Server")
   end,
 }
