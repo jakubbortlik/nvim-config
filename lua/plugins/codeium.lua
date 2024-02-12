b369245ec41ef4c6bcf3410d5a93d6ffdd58583c
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-02-12 12:58:10 +0100

    feat: update codeium config

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,11 +1,13 @@
+local nmap = require("utils").nmap
+
 M = {
   "Exafunction/codeium.vim",
-  event = 'BufEnter',
+  event = "BufEnter",
   config = function()
     vim.keymap.set("i", "<M-p>", function()
       return vim.fn["codeium#Accept"]()
     end, { expr = true })
-    vim.keymap.set("n", "<leader>ct", "<cmd>CodeiumToggle<cr>", { desc = "Toggle Codeium" })
+    nmap("<leader>ct", "<cmd>CodeiumToggle<cr>", "Toggle Codeium")
     vim.g.codeium_no_map_tab = true
     vim.g.codeium_enabled = false
   end,
