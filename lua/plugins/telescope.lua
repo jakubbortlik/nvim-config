9b50992fae659b34a9a3c875050b31602b312d40
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-02-12 12:58:10 +0100

    refactor(telescope): make "telescope-fzf-native.nvim" a dependency of telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,18 @@ local M = {
       "nvim-lua/plenary.nvim",
       "nyarthan/telescope-code-actions.nvim",
       'nvim-telescope/telescope-ui-select.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
     },
     config = function()
       local telescope = require("telescope")
@@ -53,18 +65,6 @@ local M = {
     end,
   },
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = "make",
-    cond = function()
-      return vim.fn.executable("make") == 1
-    end,
-  },
 }
 
 return M
