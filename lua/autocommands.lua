c04151d0c6226dcf9e16e1f727e1b5575ea60be5
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-02-12 12:58:10 +0100

    feat(autocmd): add autocmd for highlighting selected text

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,6 +1,6 @@
 -- Terminal autocommands
 local term_id = vim.api.nvim_create_augroup("Terminal", {
-  clear = false
+  clear = true
 })
 
 -- always enter insert mode when switching to a terminal window
@@ -29,12 +29,22 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
 })
 
 local python_id = vim.api.nvim_create_augroup("Python", {
-  clear = false
+  clear = true
 })
 vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
   group = python_id,
   pattern = { "*py" },
   callback = function()
     vim.keymap.set("n", "<leader>ra", "<cmd>vnew term://python "..vim.fn.expand("%:p").."<cr>", { desc = "Run current file with Python" })
+  end
+})
+
+local editor_id = vim.api.nvim_create_augroup("Editor", {
+  clear = true
+})
+vim.api.nvim_create_autocmd({"TextYankPost"}, {
+  group = editor_id,
+  callback  = function()
+    vim.highlight.on_yank({higroup="IncSearch", timeout=250})
   end
 })
