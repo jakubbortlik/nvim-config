1c0d512519d037919cda5183d9ffc08d891d3648
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-04-10 16:53:40 +0200

    feat: update gitlab.nvim config

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -30,52 +30,30 @@ return {
         },
       },
       discussion_tree = {
-        size = "25%",                                    -- Size of split
+        jump_to_reviewer = "a",
+        size = "25%", -- Size of split
         position = "right",
-        toggle_resolved = "s",
+        keep_current_open = true,
+        expanded_by_default = { resolved = false, unresolved = true },
       },
-      discussion_sign_and_diagnostic = {
+      discussion_signs = {
         skip_resolved_discussion = true,
+        virtual_text = true,
       },
       popup = {
         exit = "q",
         perform_action = "ZZ",
         opacity = 0.85,
         width = "60%",
-        backup_register = "+",
+        temp_registers = { '"', "g", "+" },
       },
       create_mr = {
         title_input = {
-          width = 120,
-        }
-      }
-    })
-    local create_mappings = function()
-      nmap("c", gitlab.create_comment, "Gitlab Create Comment", true)
-      vmap("c", gitlab.create_multiline_comment, "Gitlab Multiline Comment", true)
-      vmap("s", gitlab.create_comment_suggestion, "Gitlab Suggestion", true)
-      nmap("C", gitlab.create_note, "Gitlab Create note", true)
-      nmap("td", gitlab.toggle_discussions, "Gitlab Toggle Discussions", true)
-      nmap("aa", gitlab.add_assignee, "Gitlab Add Assignee", true)
-      nmap("da", gitlab.delete_assignee, "Gitlab Delete Assignee", true)
-      nmap("ar", gitlab.add_reviewer, "Gitlab Add Reviewer", true)
-      nmap("dr", gitlab.delete_reviewer, "Gitlab Delete Reviewer", true)
-      nmap("P", gitlab.pipeline, "Gitlab Pipeline", true)
-      nmap("O", gitlab.open_in_browser, "Gitlab Open in browser", true)
-      nmap(
-        "m",
-        gitlab.move_to_discussion_tree_from_diagnostic,
-        "Move to discussion",
-        true
-      )
-    end
-    local id_gitlab = vim.api.nvim_create_augroup("gitlab", {
-      clear = true,
-    })
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter" }, {
-      group = id_gitlab,
-      pattern = "diffview:///*",
-      callback = create_mappings,
+          width = vim.fn.winwidth(0) > 120 and 120 or vim.fn.winwidth(0) - 10,
+        },
+        squash = false,
+        delete_branch = true,
+      },
     })
     nmap("glr", gitlab.review, "Gitlab Review")
     nmap("gls", gitlab.summary, "Gitlab Summary")
