315d6cee73c8da7163ea2527d11011b17a4b671c
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-12-11 13:09:35 +0100

    feat(fugitiveblame): move to last window when quitting

--- after/ftplugin/fugitiveblame.lua
+++ after/ftplugin/fugitiveblame.lua
@@ -1,1 +1,10 @@
-vim.keymap.set("n", "q", "<cmd>close<cr>", { desc = "Close the Git blame window", buffer = true })
+vim.keymap.set(
+  "n",
+  "q",
+  function()
+    local winid = vim.api.nvim_get_current_win()
+    vim.cmd.wincmd("w")
+    vim.api.nvim_win_close(winid, true)
+  end,
+  { desc = "Close the Git blame window", buffer = true }
+)
