f2cbc15e9b4eb73f4003f1336ed9467634152da2
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-02-11 14:53:20 +0100

    fix(oil.nvim): use better keymaps

---
+++ lua/plugins/markdown-table-mode.lua
@@ -0,0 +1,10 @@
+return {
+  'Kicamon/markdown-table-mode.nvim',
+  ft = { "markdown" },
+  opts = {
+    options = {
+      pad_separator_line = true, -- add space in separator line
+    },
+  },
+  config = true,
+}
