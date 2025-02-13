a4863001fb56f5b55bd67a3423cf4f1b714db05c
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-02-13 16:41:17 +0100

    fix: set autocommand only in the COMMIT_EDITMSG buffer

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -73,6 +73,17 @@ vim.api.nvim_create_autocmd("User", {
     end
   end,
 })
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = editor_id,
+  pattern = "COMMIT_EDITMSG",
+  callback = function()
+    if vim.fn.line('.') == 1 and vim.fn.col('.') == 1 then
+      vim.schedule(function()
+        vim.fn.complete(1, {'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'})
+      end)
+    end
+  end
+})
 
 vim.api.nvim_create_autocmd('User', {
   pattern = 'GitSignsChanged',
