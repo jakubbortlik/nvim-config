156cdeb890424351bdf1a04c6dc087281c13fcff
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-08 07:45:56 +0200

    fix(flash): don't create operator mapping for jump

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -174,7 +174,7 @@ return {
     },
     -- stylua: ignore
     keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "s",     mode = { "n", "x" }, function() require("flash").jump() end,              desc = "Flash" },
       { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
