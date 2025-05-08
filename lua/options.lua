b6f0c4b55fcee6cac74075d74f2b08cd8ec30918
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-08 08:15:55 +0200

    fix(grepprg): use explicit options

--- lua/options.lua
+++ lua/options.lua
@@ -6,7 +6,7 @@ vim.opt.cursorline = true
 vim.opt.diffopt:append({"linematch:60"})
 vim.opt.expandtab = true
 vim.opt.fileformat = "unix"
-vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case --sort=path"
+vim.opt.grepprg = "rg --color=never --no-heading --with-filename --line-number --column --smart-case --sort=path"
 vim.opt.grepformat = "%f:%l:%c:%m"
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.opt.hlsearch = false
