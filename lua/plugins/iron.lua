674f96895afbca1d010240423308962d97aeaf7d
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-08 08:15:08 +0200

    feat: add iron REPL

---
+++ lua/plugins/iron.lua
@@ -0,0 +1,38 @@
+-- A basic REPL that opens up as a horizontal split
+return {
+  "Vigemus/iron.nvim",
+  keys = {
+    { "<leader>re", vim.cmd.IronRepl, desc = "󱠤 Toggle REPL" },
+    { "+", mode = { "n", "x" }, desc = "󱠤 Send-to-REPL Operator" },
+    { "++", desc = "󱠤 Send Line to REPL" },
+    { "<leader>rw", function() vim.cmd.IronWatch("file") end, desc = "󱠤 REPL watch file" },
+    { "<leader>ru", function() require("iron.core").unwatch(0) end, desc = "󱠤 REPL unwatch file" },
+  },
+  -- irons's setup call is `require("iron.core").setup`
+  main = "iron.core",
+  opts = {
+    keymaps = {
+      send_line = "++",
+      visual_send = "+",
+      send_motion = "+",
+      mark_visual = "<leader>m",
+      cr = "<leader><cr>",
+      interrupt = "<leader>ri",
+      exit = "<leader>rq",
+      clear = "<leader>rl",
+    },
+    config = {
+      repl_open_cmd = "horizontal bot 15 split",
+      -- Define which binary to use for the REPL.
+      repl_definition = {
+        python = {
+          command = function()
+            local ipythonAvailable = vim.fn.executable("ipython") == 1
+            local binary = ipythonAvailable and "ipython" or "python3"
+            return { binary}
+          end,
+        },
+      },
+    },
+  },
+}
