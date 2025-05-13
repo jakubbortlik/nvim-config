28a5a43a19a2d9b69ea08a2f560e519b0be7e84f
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-13 14:30:05 +0200

    feat(dressing): use telescope for vim.ui.select

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,10 +1,15 @@
 return {
   "stevearc/dressing.nvim",
-  config = function()
-    require("dressing").setup({
-      input = {
-        enabled = true,
-      },
-    })
-  end
+  opts = {
+    select = {
+      enabled = true,
+      telescope = require('telescope.themes').get_dropdown({
+        layout_config = {
+          width = function(_, max_columns, _)
+            return math.min(max_columns + 4, 180)
+          end
+        }
+      })
+    },
+  },
 }
