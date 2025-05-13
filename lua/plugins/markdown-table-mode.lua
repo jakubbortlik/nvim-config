9e9a9c8f22f1400c2f05b3ff198914688213ace9
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-13 10:31:07 +0200

    fix(markdown-table-mode): update config

--- lua/plugins/markdown-table-mode.lua
+++ lua/plugins/markdown-table-mode.lua
@@ -1,10 +1,18 @@
 return {
-  'Kicamon/markdown-table-mode.nvim',
-  ft = { "markdown" },
-  opts = {
-    options = {
-      pad_separator_line = true, -- add space in separator line
-    },
-  },
-  config = true,
+  "Kicamon/markdown-table-mode.nvim",
+  cmd = { "Mtm" },
+  config = function()
+    require("markdown-table-mode").setup({
+      filetype = {
+        "*.md",
+        "*.mdx",
+      },
+      options = {
+        insert = true,             -- when typeing "|"
+        insert_leave = true,       -- when leaveing insert
+        pad_separator_line = true, -- add space in separator line
+        alig_style = "default",    -- default, left, center, right
+      },
+    })
+  end,
 }
