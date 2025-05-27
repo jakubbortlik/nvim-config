ec192b2caccb23b45707a2ca26b87ad94791c288
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-27 07:45:29 +0200

    fix(treesitter): remove unnecessary events for lazyloading
    nvim-treesitter doesn't support lazy loading

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
