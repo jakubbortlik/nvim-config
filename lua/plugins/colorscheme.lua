8488fbe2ac5f543ab6e580d04ec02b6be72678c9
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-06-06 15:29:15 +0200

    fix: clean up colorscheme config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,3 @@
-local set_hl = vim.api.nvim_set_hl
-
-vim.api.nvim_create_autocmd('ColorScheme', {
-  callback = function ()
-    set_hl(0, "ColorColumn", { link = "CursorLine" })
-    set_hl(0, "@variable.builtin.python", { fg = "#e46876", italic = true })
-    set_hl(0, "@variable.python", { link = "Identifier" })
-    set_hl(0, "@parameter.python", { fg = "#5fafff" })
-    set_hl(0, "@type.builtin.python", { fg = "#ff5fff" })
-    set_hl(0, "@constant.builtin.python", { link = "Constant" })
-    set_hl(0, "QuickFixLine", { link = "WarningMsg" })
-    set_hl(0, "NeoTreeGitUntracked", { fg = "#76946a" })
-    set_hl(0, "IBLIndent", { bg="NONE", fg="#1a1a1a", nocombine=true })
-    set_hl(0, "MatchParen", { bg="#363646", fg="#ff9e3b" })
-    set_hl(0, "CursorLine", { bg="#161616" })
-    set_hl(0, "Substitute", { bg="#c34043", fg="#000000"})
-    set_hl(0, "DiffAdd",  { bg="#182015" })
-    set_hl(0, "DiffChange", { bg="#151525" })
-    set_hl(0, "SnacksPickerListCursorLine", { link = "Cursorline" })
-  end
-})
-
-
 return {
   {
     "rebelot/kanagawa.nvim",
@@ -34,7 +11,19 @@ return {
           light = "lotus",
         },
         colors = {
-          theme = { all = { ui = { bg_gutter = "none" }, }, },
+          theme = {
+            wave = {
+              ui = {
+                bg = "none",
+                bg_p1 = "#161616",
+                bg_p2 = "#161616",
+              },
+              diff = {
+                change = "#151527",
+              }
+            },
+            all = { ui = { bg_gutter = "none" }, },
+          },
         },
         overrides = function(colors)
           local theme = colors.theme
@@ -42,8 +31,11 @@ return {
             NormalFloat = { bg = "none" },
             FloatBorder = { bg = "none" },
             FloatTitle = { bg = "none" },
-            TelescopeBorder = { bg = "none" },
             WinSeparator = { fg = theme.ui.nontext },
+            IBLIndent = { bg="NONE", fg="#1a1a1a", nocombine=true },
+            ["@type.builtin.python"] = { fg = "#cc4dcc" },
+            QuickFixLine = { link = "WarningMsg" },
+            Substitute = { bg="#c34043", fg="#000000"},
 
             -- Save a hlgroup with dark background and dimmed foreground
             -- so that you can use it where your still want darker windows.
@@ -57,7 +49,7 @@ return {
           }
         end,
       })
-      vim.cmd.colorscheme("kanagawa-wave")
+      vim.cmd.colorscheme("kanagawa")
     end,
   },
 }
