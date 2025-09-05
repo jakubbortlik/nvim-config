6efe222899c56d4ab4d48898a2bcb15f4b2fef6c
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-09-06 00:01:34 +0200

    feat(fugitive): add keymap for opening in right-most vertical split

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,1 +1,2 @@
 vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = true, desc = "Close the Git summary window" })
+vim.keymap.set("n", "go", "oL", { buffer = true, desc = "Open in vertical split", remap = true })
