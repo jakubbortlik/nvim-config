c43b5ca1935e158c7dadeab4c04b8d6b6cee7d01
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-10-13 23:46:37 +0200

    feat: add neogit plugin

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,34 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "folke/snacks.nvim",
+  },
+  keys = {
+    { "gs", "<cmd>Neogit<cr>", desc = "Open Neo[g]it [s]tatus in new tab" },
+    { "gS", "<cmd>Neogit kind=vsplit<cr>", desc = "Vsplit Neo[g]it [s]tatus" },
+  },
+  opts = {
+    commit_editor = {
+      staged_diff_split_kind = "auto",
+    },
+    integrations = {
+      diffview = true,
+      snacks = true,
+    },
+    git_services = {
+      ["gitlab.cloud.phonexia.com"] = {
+        pull_request = "https://gitlab.cloud.phonexia.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+        commit = "https://gitlab.cloud.phonexia.com/${owner}/${repository}/-/commit/${oid}",
+        tree = "https://gitlab.cloud.phonexia.com/${owner}/${repository}/-/tree/${branch_name}?ref_type=heads",
+      },
+    },
+    mappings = {
+      status = {
+        ["K"] = "OpenOrScrollUp",
+        ["J"] = "OpenOrScrollDown",
+      },
+    },
+  },
+}
