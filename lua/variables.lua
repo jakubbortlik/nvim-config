35bf4e06776850672759e93d0eaca99fd18c09a9
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-10-13 23:27:46 +0200

    chore: remove unnecessary Python global variable

--- lua/variables.lua
+++ lua/variables.lua
@@ -1,8 +1,3 @@
 -- Define leader before plugins are required otherwise wrong leader will be used
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
--- Get correct python3_host_prog on different machines
--- TODO: make sure that pynvim is installed
-local conda_prefix = string.gsub(vim.fn.expand("$CONDA_PREFIX"), "/envs/.*", "")
-vim.g.python3_host_prog = conda_prefix .. "/envs/neovim/bin/python3"
