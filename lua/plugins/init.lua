cf34c035bfbfa233e6f5e84a823f127d1c4ead56
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-10-14 11:54:09 +0200

    chore: remove unused Flash plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,21 +38,6 @@ return {
       "TmuxNavigateUp",
     },
   },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {
-      label = { after = false, before = { 0, 0 } },
-      modes = { search = { enabled = false, highlight = { backdrop = true } } },
-    },
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "x" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "r",     mode = "o",          function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },      function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-    },
-  },
 
   -- Unicode tables and digraphs expansion
   {
