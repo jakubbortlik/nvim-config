3ffd1a66b5a093af1afeea9ad67632fa9e582475
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-02-05 11:31:17 +0100

    fix(python): get width for fmt from buffer

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,6 +1,7 @@
+local textwidth = vim.bo.textwidth
 vim.keymap.set(
   "n",
   "<leader>f88",
-  [[vib:s/\v(^\s+)"(.*)"$/\1\2/<CR>vib:!fmt -w 85<CR>vib:s/\v(^\s+)(.*)/\1"\2 "<CR>$X]],
-  { desc = "Format parenthesized string to 88 chars"}
+  [[vib:s/\v(^\s+)"(.*)"$/\1\2/<CR>0vib:!fmt -w ]] .. textwidth - 3 .. [[<CR>vib:s/\v(^\s+)(.*)/\1"\2 "<CR>$X:update<cr>]],
+  { desc = string.format("Format parenthesized string to %d chars", textwidth)}
 )
