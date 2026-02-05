016cefa8292ababcef7f1cd734d93ec9e547d1ef
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-02-05 11:44:03 +0100

    feat(debugprint): add correction mapping

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -21,5 +21,10 @@ return {
     {"g?sv", mode = {"n", "x"}},
     {"g?sp", mode = {"n", "x"}},
     {"<leader>s<c-d>", "<cmd>Debugprint search<cr>", mode = {"n"}},
+    {"gA",function()
+      print('DEBUGPRINT[424]: debugprint.lua:24 (after gA,function())')
+      vim.fn.search("DEBUGPRINT")
+      vim.cmd.normal("ddk%p==")
+    end, mode = {"n"}, desc = "Move debugprint after multiline statement"},
   },
 }
