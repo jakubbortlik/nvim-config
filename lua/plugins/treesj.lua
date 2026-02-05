cca1df14860811bd7dc7165967fc6bd330c522c6
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-02-05 11:39:47 +0100

    fix(treesj): remove unused keymap

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,7 +1,6 @@
 return {
   "Wansmer/treesj",
   keys = {
-    { '<leader>jt', "<cmd>TSJToggle<cr>", desc =  "trees[j] [t]oggle" },
     { '<leader>jo', "<cmd>TSJJoin<cr>", desc = "treesj [jo]in" },
     { '<leader>js', "<cmd>TSJSplit<cr>", desc = "trees[j] [s]plit" },
   },
