0c21e09ab16e49f77d5b7e72f6ae88b09b216ee2
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-02-25 14:37:37 +0100

    fix(navic): add preference for pylsp

--- lua/plugins/navbuddy.lua
+++ lua/plugins/navbuddy.lua
@@ -1,13 +1,17 @@
 -- Navigation in a popup window using LSP
+local lsp_opts = { auto_attach = true, preference = { "pylsp" } }
 return {
   "hasansujon786/nvim-navbuddy",
   dependencies = {
     "neovim/nvim-lspconfig",
-    "SmiteshP/nvim-navic",
+    { "SmiteshP/nvim-navic", opts = { lsp = lsp_opts } },
     "MunifTanjim/nui.nvim",
     "folke/snacks.nvim",
   },
   keys = { { "<leader>nb", "<cmd>Navbuddy<cr>", desc = "Navbuddy" } },
   cmd = "NavBuddy",
-  opts = { lsp = { auto_attach = true }, window = { border = vim.o.winborder } },
+  opts = {
+    lsp = lsp_opts,
+    window = { border = vim.o.winborder },
+  },
 }
