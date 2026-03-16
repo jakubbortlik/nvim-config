e53804199b0daaa901bf6841770e55ecef608ee6
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-03-16 13:39:37 +0100

    docs: remove comment

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,9 +8,6 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = false },
     dim = { enabled = true, animate = { enabled = false }},
