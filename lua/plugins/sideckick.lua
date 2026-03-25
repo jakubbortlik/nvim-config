f581ee093155ea5646ff4acfcd2a69d1d0730731
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-03-25 16:57:08 +0100

    feat(sidekick): enable toggling terminal at the bottom

--- lua/plugins/sideckick.lua
+++ lua/plugins/sideckick.lua
@@ -20,7 +20,29 @@ return {
     {
       "<c-.>",
       function()
-        require("sidekick.cli").toggle({ name = "claude", focus = true })
+        local new_layout = vim.v.count1 == 1 and "right" or "bottom"
+        require("sidekick.config").cli.win.layout = new_layout
+        require("sidekick.cli.state").with(function(state, attached)
+          if state.terminal then
+            state.terminal.opts.layout = new_layout
+            if attached then
+              state.terminal:show()
+              state.terminal:focus()
+            elseif state.terminal:is_open() then
+              state.terminal:hide()
+              state.terminal:show()
+              state.terminal:focus()
+            else
+              state.terminal:toggle()
+              if state.terminal:is_open() then
+                state.terminal:focus()
+              end
+            end
+          end
+        end, {
+          attach = true,
+          filter = { name = "claude" },
+        })
       end,
       desc = "Sidekick Toggle Claude",
       mode = { "n", "t", "i", "x" },
