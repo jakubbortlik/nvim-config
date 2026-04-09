fbc0f41e06edd53253739f27156a0eabbcc19e04
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-04-09 17:34:49 +0200

    style(debugprint): format

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -4,27 +4,32 @@ return {
     keymaps = {
       normal = {
         delete_debug_prints = "g?d",
-      }
-    }
+      },
+    },
   },
   dependencies = {
     "folke/snacks.nvim",
   },
   keys = {
-    {"g?d"},
-    {"g?v", mode = {"n", "x"}},
-    {"g?V", mode = {"n", "x"}},
-    {"g?p"},
-    {"g?P"},
-    {"g?o"},
-    {"g?O"},
-    {"g?sv", mode = {"n", "x"}},
-    {"g?sp", mode = {"n", "x"}},
-    {"<leader>s<c-d>", "<cmd>Debugprint search<cr>", mode = {"n"}},
-    {"gA",function()
-      print('DEBUGPRINT[424]: debugprint.lua:24 (after gA,function())')
-      vim.fn.search("DEBUGPRINT")
-      vim.cmd.normal("ddk%p==")
-    end, mode = {"n"}, desc = "Move debugprint after multiline statement"},
+    { "g?d" },
+    { "g?v", mode = { "n", "x" } },
+    { "g?V", mode = { "n", "x" } },
+    { "g?p" },
+    { "g?P" },
+    { "g?o" },
+    { "g?O" },
+    { "g?sv", mode = { "n", "x" } },
+    { "g?sp", mode = { "n", "x" } },
+    { "<leader>s<c-d>", "<cmd>Debugprint search<cr>", mode = { "n" } },
+    {
+      "gA",
+      function()
+        print("DEBUGPRINT[424]: debugprint.lua:24 (after gA,function())")
+        vim.fn.search("DEBUGPRINT")
+        vim.cmd.normal("ddk%p==")
+      end,
+      mode = { "n" },
+      desc = "Move debugprint after multiline statement",
+    },
   },
 }
