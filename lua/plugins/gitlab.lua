8e2377772f5f326e0a3a8fe9fdae2db45cb0d35a
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-09-24 08:02:45 +0200

    fix(gitlab): track various in-progress changs

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -38,6 +38,7 @@ return {
     { "glD", desc = "Toggle MR comment draft mode" },
     { "glL", desc = "Open gitlab.nvim.log in a new tab" },
     { "glM", desc = "Merge MR" },
+    { "glm", desc = "Set MR to auto-merge" },
     { "glP", desc = "Publish all MR comment drafts" },
     { "glR", desc = "Revoke approval" },
     { "glS", desc = "Start Gitlab review" },
@@ -52,6 +53,7 @@ return {
     { "gln", desc = "Create MR note" },
     { "glo", desc = "Open MR in browser" },
     { "glp", desc = "Show MR pipeline status" },
+    { "gl<c-p>", desc = "Show CI pipeline with glab" },
     { "glra", desc = "Add MR reviewer" },
     { "glrd", desc = "Delete MR reviewer" },
     { "gls", desc = "Show MR summary" },
@@ -60,6 +62,7 @@ return {
   config = function()
     local gitlab = require("gitlab")
     local gitlab_server = require("gitlab.server")
+    ---@type GitlabSettings
     gitlab.setup({
       debug = { -- Which values to log
         request = false,
@@ -71,9 +74,14 @@ return {
         help_nowait = true,
         popup = {
           perform_linewise_action_nowait = true,
+          next_field = "g<tab>", -- Cycle to the next field. Accepts |count|.
+          prev_field = "g<S-tab>", -- Cycle to the previous field. Accepts |count|.
         },
         reviewer = {
           create_comment_nowait = true,
+          create_suggestion_with_preview_nowait = true,
+          create_suggestion_with_preview = "s",
+          create_suggestion = "S",
         },
         discussion_tree = {
           switch_view = "C",
@@ -86,13 +94,29 @@ return {
           imply_local = true,
         },
       },
+      mergeability_checks = {
+        statuses = {
+          INACTIVE = false,
+        },
+      },
       discussion_tree = {
+        indent_width = 2, -- Display columns that one level of nesting indents by. Indentation is drawn as virtual text, so it never enters the buffer and is not copied along with note text
+        winopts = {
+          linebreak = false,
+          breakindent = true,
+          showbreak = "X",
+        },
+        expanders = { -- Discussion tree icons
+          expanded = " ", -- Icon for expanded discussion thread
+          collapsed = " ", -- Icon for collapsed discussion thread
+        },
         size = discussion_tree_size, -- Size of split
         position = discussion_tree_position,
         keep_current_open = true,
         expanded_by_default = { resolved = false, unresolved = true },
         draft_mode = true,
         sort_by = "original_comment",
+        reviewer_follows_discussion = true,
       },
       emojis = {
         formatter = function(val)
@@ -102,7 +126,7 @@ return {
           emoji = emoji:gsub("^([\226][\156][\141])([\239][\184][\143]?)", tmux and " %1%2 " or "%1%2 ") -- writing hand
           emoji = emoji:gsub("[\240][\159][\143][\187-\191]", "") -- remove skin tones
           emoji = emoji:gsub(tmux and "^ " or "^" .. "([\240][\159][\135][\166-\191])  ", tmux and "  %1" or "%1") -- prepend flags with a space
-          return string.format("%s  %s  %s", emoji, val.shortname, val.name)
+          return string.format("%s  %s | %s (%s)", emoji, val.shortname, val.name, val.category)
         end,
       },
       discussion_signs = {
@@ -141,8 +165,10 @@ return {
       },
     })
 
-    u.nmap("gl<C-a>", "<cmd>lua Snacks.terminal.open('glab mr approvers', {auto_close = false, win = {width = 90, height = 30, border = 'rounded'}})<cr>", "Show eligible approvers")
-    u.nmap("glp", "<cmd>lua Snacks.terminal.open('glab ci view', {win = {width = 190}})<cr>", "Show current CI pipeline")
+    u.nmap("gl<C-a>", function()
+      Snacks.terminal.open('glab mr approvers', {auto_close = false, win = {width = 90, height = 30, border = 'rounded'}})
+    end, "Show eligible approvers")
+    u.nmap("gl<c-p>", "<cmd>lua Snacks.terminal.open('glab ci view', {win = {width = 190}})<cr>", "Show CI pipeline with glab")
     u.nmap("glL", function()
       vim.cmd.tabnew()
       vim.cmd.edit(gitlab.state.settings.log_path)
@@ -162,20 +188,21 @@ return {
       vim.notify("Rebuilding Gitlab Go server.")
       gitlab_server.build(true)
     end, "Rebuild the Gitlab Go server")
+    u.nmap("gle", require("gitlab").create_comment, "Create comment on current line")
+    u.nmap("gl<c-e>", function()
+      require("gitlab").create_comment({with_suggestion=true})
+    end, "Create comment on current line")
+    u.vmap("gl<c-e>", function()
+      require("gitlab").create_comment({with_suggestion=true})
+    end, "Create comment on current line")
+    u.vmap("gle", require("gitlab").create_multiline_comment, "Create comment on current selection")
+    u.vmap("glE", require("gitlab").create_comment_suggestion, "Create comment suggestion on current selection")
     u.nmap("gl<c-s>", function()
       vim.cmd.tabnew()
       local settings = vim.inspect(require("gitlab").state.settings)
       vim.api.nvim_put(vim.split(settings, "\n"), "l", true, true)
       vim.cmd.only()
+      vim.o.buftype = "nofile"
     end, "Print gitlab.nvim settings")
-    u.nmap("gl<C-r>", function()
-      gitlab_server.restart(function()
-        if #vim.api.nvim_list_tabpages() == 1 then
-          vim.cmd.tabnew()
-        end
-        vim.cmd.tabclose(vim.api.nvim_tabpage_get_number(require("gitlab.reviewer").tabnr))
-        gitlab.review()
-      end)
-    end, "Gitlab Restart Server")
   end,
 }
