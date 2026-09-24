87dbdaf1d979e2cd0107aa7192ada535586f43ed
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-09-24 07:50:44 +0200

    fix(refactoring): create mappings the new way

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,32 +1,67 @@
+vim.keymap.set({ "n", "x" }, "<leader>re", function()
+  return require("refactoring").extract_func()
+end, { desc = "Extract Function", expr = true })
+vim.keymap.set("n", "<leader>ree", function()
+  return require("refactoring").extract_func() .. "_"
+end, { desc = "Extract Function (line)", expr = true })
+
+vim.keymap.set({ "n", "x" }, "<leader>rv", function()
+  return require("refactoring").extract_var()
+end, { desc = "Extract Variable", expr = true })
+vim.keymap.set("n", "<leader>rvv", function()
+  return require("refactoring").extract_var() .. "_"
+end, { desc = "Extract Variable (line)", expr = true })
+
+vim.keymap.set({ "n", "x" }, "<leader>ri", function()
+  return require("refactoring").inline_var()
+end, { desc = "Inline Variable", expr = true })
+vim.keymap.set({ "n", "x" }, "<leader>rI", function()
+  return require("refactoring").inline_func()
+end, { desc = "Inline function", expr = true })
+
+vim.keymap.set({ "n", "x" }, "<leader>rs", function()
+  return require("refactoring").select_refactor()
+end, { desc = "Select refactor" })
+
+vim.keymap.set({ "x", "n" }, "<leader>pv", function()
+  return require("refactoring.debug").print_var { output_location = "below" } .. "iw"
+end, { desc = "Debug print var below", expr = true })
+
+vim.keymap.set({ "x", "n" }, "<leader>pV", function()
+  return require("refactoring.debug").print_var { output_location = "above" } .. "iw"
+end, { desc = "Debug print var above", expr = true })
+
+vim.keymap.set({ "x", "n" }, "<leader>pe", function()
+  return require("refactoring.debug").print_exp { output_location = "below" }
+end, { desc = "Debug print exp below", expr = true })
+vim.keymap.set("n", "<leader>pee", function()
+  return require("refactoring.debug").print_exp { output_location = "below" } .. "_"
+end, { desc = "Debug print exp below", expr = true })
+
+vim.keymap.set({ "x", "n" }, "<leader>pE", function()
+  return require("refactoring.debug").print_exp { output_location = "above" }
+end, { desc = "Debug print exp above", expr = true })
+vim.keymap.set("n", "<leader>pEE", function()
+  return require("refactoring.debug").print_exp { output_location = "above" } .. "_"
+end, { desc = "Debug print exp above", expr = true })
+
+vim.keymap.set("n", "<leader>pP", function()
+  return require("refactoring.debug").print_loc { output_location = "above" }
+end, { desc = "Debug print location", expr = true })
+vim.keymap.set("n", "<leader>pp", function()
+  return require("refactoring.debug").print_loc { output_location = "below" }
+end, { desc = "Debug print location", expr = true })
+
+vim.keymap.set({ "x", "n" }, "<leader>pc", function()
+  return require("refactoring.debug").cleanup { restore_view = true } .. "gG"
+end, { desc = "Debug print clean", expr = true, remap = true })
+
 return {
   "ThePrimeagen/refactoring.nvim",
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    "lewis6991/async.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = function()
-    local refactoring = require("refactoring")
-    refactoring.setup({})
-    vim.keymap.set("x", "<leader>re", ":Refactor extract ")
-    vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
-
-    vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
-
-    vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
-
-    vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
-
-    vim.keymap.set("n", "<leader>reb", ":Refactor extract_block")
-    vim.keymap.set("n", "<leader>ref", ":Refactor extract_block_to_file")
-
-    vim.keymap.set({ "x", "n" }, "<leader>rp", function()
-      refactoring.debug.print_var({ below = true })
-    end, { desc = "Print var below" })
-    vim.keymap.set({ "x", "n" }, "<leader>rP", function()
-      refactoring.debug.print_var({ below = false })
-    end, { desc = "Print var above" })
-    vim.keymap.set("n", "<leader>rc", function()
-      refactoring.debug.cleanup({})
-    end, { desc = "Remove Refactoring debugprints" })
-  end,
+  lazy = false,
+  config = true
 }
