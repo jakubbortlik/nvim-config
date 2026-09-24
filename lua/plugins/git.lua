1c4dc7b7a06a7feda1f9ae32f54ab0845b588070
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-09-24 07:39:56 +0200

    feat(diffview): add jj preferred adapter and default layout

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -53,6 +53,8 @@ local M = {
       "DiffviewToggleFiles",
     },
     opts = {
+      view = { default = { layout = "diff2_horizontal" } },
+      preferred_adapter = "jj",  -- Preferred VCS adapter ('git'|'hg'|'jj'|'p4'). Tried first when detecting repos.
       persist_selections = {
         enabled = true,
       },
